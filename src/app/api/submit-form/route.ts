cdf10db52f678319d5861548cc9637fcd5baea7f
Daniil <1DaniilMedvedev@gmail.com>
2026-03-22 17:02:47 +0100

    fix

--- src/app/api/submit-form/route.ts
+++ src/app/api/submit-form/route.ts
@@ -4,21 +4,14 @@ export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
 
-        const SCRIPT_URL = process.env.GOOGLE_SCRIPT_URL;
-
-        // Проверка что переменная есть
-        if (!SCRIPT_URL) {
-            return NextResponse.json({ error: "GOOGLE_SCRIPT_URL is not set" }, { status: 500 });
-        }
-
-        const res = await fetch(SCRIPT_URL, {
+        const res = await fetch("https://script.google.com/macros/s/AKfycbwpJplDVZ5870X6bBZLrRVgC7Ixgel53WACJpkhrPNxW6EqyAACmqY3Zcmlo2JtG9nq/exec", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(body),
         });
 
-        const data = await res.json();
-        return NextResponse.json({ status: "ok", data });
+        const text = await res.text();
+        return NextResponse.json({ status: "ok", response: text });
 
     } catch (err: any) {
         return NextResponse.json({ error: err.message }, { status: 500 });
