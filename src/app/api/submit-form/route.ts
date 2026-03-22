c2d2400b28f2f9e58d1cc33482561a246fbcd195
Daniil <1DaniilMedvedev@gmail.com>
2026-03-22 17:08:06 +0100

    fix

--- src/app/api/submit-form/route.ts
+++ src/app/api/submit-form/route.ts
@@ -4,7 +4,7 @@ export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
 
-        const res = await fetch("https://script.google.com/macros/s/AKfycbwpJplDVZ5870X6bBZLrRVgC7Ixgel53WACJpkhrPNxW6EqyAACmqY3Zcmlo2JtG9nq/exec", {
+        const res = await fetch("https://script.google.com/macros/s/AKfycbxWQmcUR0vq8BKenVOFCZeg53UtYAIghBO_6hWnhbyQUcug0nNQO9IBO9hSGraQZ-L0/exec", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(body),
